397fe4b0ca4c3bd3bf9b70ed6545e9d31d0114cc
Prince Kumar <68233983+pprince493@users.noreply.github.com>
2023-04-19 11:36:24 +0530

    Commit devosassignment

---
+++ DevopsAssignment1/DevopsAssignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevopsAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
